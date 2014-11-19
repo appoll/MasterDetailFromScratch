45994763dcf9c1894b29eb87fb3b8bf7a7d9b0a3
Paul Anton <paul.antton@gmail.com>
2014-11-20 01:44:03 +0200

    Added AsyncTask

--- app/src/main/java/antton/paul/masterdetailfromscratch/MainListActivity.java
+++ app/src/main/java/antton/paul/masterdetailfromscratch/MainListActivity.java
@@ -1,18 +1,41 @@
 package antton.paul.masterdetailfromscratch;
 
-import android.app.Activity;
 import android.app.ListActivity;
+import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainListActivity extends ListActivity {
 
+    protected String[] mBlogPostTitles;
+    public static final int NUMBER_OF_POSTS = 20;
+    public static final String TAG = MainListActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
+
+        GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+        getBlogPostsTask.execute();
+        /*
+        Resources resources = getResources();
+          mBlogPostTitles = resources.getStringArray(R.array.android_names);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mBlogPostTitles);
+        setListAdapter(adapter);
+*/
+        String message = getString(R.string.no_items);
+        Toast.makeText(this,message, Toast.LENGTH_LONG).show();
     }
 
 
@@ -37,4 +60,32 @@ public class MainListActivity extends ListActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class GetBlogPostsTask extends AsyncTask<Object,Void,String>{
+
+        @Override
+        protected String doInBackground(Object[] objects) {
+            int responseCode = -1;
+            try {
+                URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+                HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+                connection.connect();
+
+                responseCode = connection.getResponseCode();
+                Log.i(TAG, "Code: " + responseCode);
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "Exception caught: ", e);
+            } catch (IOException e) {
+                Log.e(TAG, "Exception caught: ", e);
+            } catch (Exception e) {
+                Log.e(TAG, "Exception caught: ", e);
+            }
+
+            return "Code: " + responseCode;
+        }
+
+        }
 }
+
+
+
